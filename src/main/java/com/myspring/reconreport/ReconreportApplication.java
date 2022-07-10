03dccae4dc58d7bb5ac9a16a1b9813815c934379
Rajasingh Samuel <rssambbb0123@gmail.com>
2022-07-10 19:38:50 +0530

    first commit

---
+++ src/main/java/com/myspring/reconreport/ReconreportApplication.java
@@ -0,0 +1,13 @@
+package com.myspring.reconreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconreportApplication.class, args);
+	}
+
+}
